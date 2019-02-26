c6ad44a3b492d56384aa3f1faf82f4a5335297ad
liushengpeng132 <42547219+liushengpeng132@users.noreply.github.com>
2019-02-26 21:59:41 +0800

    Update 1.php

--- wxxiao/1.php
+++ wxxiao/1.php
@@ -1,9 +1,6 @@
 <?php
 
 function rewrite($filename, $data) {
-    $filenum = fopen ( $filename, "w" );
-    flock ( $filenum, LOCK_EX );
-    fwrite ( $filenum, $data );
-    fclose ( $filenum );
+    
 }
 ?>
